1345d1f4ad9f538fa0bb0c2bb8909cff8216a742
abe-pratomo <90553934+abe-pratomo@users.noreply.github.com>
2024-12-08 23:06:43 +0700

    Updated neuron_hidden.v

--- Level1/neuron_hidden.v
+++ Level1/neuron_hidden.v
@@ -2,9 +2,9 @@ module neuron_hidden(
     input signed wire [19:0] C_out, // Input from mean module
     input signed wire [19:0] D_out, // Input from dev module
 
-    output signed wire [19:0] n1_out
+    output signed wire [19:0] N1_out
 );
 
-    assign n1_out = C_out + D_out;
+    assign N1_out = C_out + D_out;
 
 endmodule
