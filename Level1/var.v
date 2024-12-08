7633dd7b48c05dee962b359cc0723bc53f1f0bd5
abe-pratomo <90553934+abe-pratomo@users.noreply.github.com>
2024-12-08 21:01:25 +0700

    Updated var.v

--- Level1/var.v
+++ Level1/var.v
@@ -3,5 +3,17 @@ module var(
     input wire signed [19:0] Wv_0, Wv_1, Wv_2, Wv_3, Wv_4, Wv_5, Wv_6, Wv_7, Wv_8, // Variance weights for each corresponding input, defined in top_level module
     input wire signed [19:0] B_var, // Bias for var module, defined in top_level module
 
-    output wire signed [19:0] D_out // Output from var module
+    output wire signed [19:0] V_out // Output from var module
 );
+
+    assign V_out = ((X_0) ? Wv_0 : 0) + // Simple conditional check whether each pixel is 1 or 0
+                   ((X_1) ? Wv_1 : 0) +
+                   ((X_2) ? Wv_2 : 0) +
+                   ((X_3) ? Wv_3 : 0) +
+                   ((X_4) ? Wv_4 : 0) +
+                   ((X_5) ? Wv_5 : 0) +
+                   ((X_6) ? Wv_6 : 0) +
+                   ((X_7) ? Wv_7 : 0) +
+                   ((X_8) ? Wv_8 : 0) + B_var;
+
+endmodule
