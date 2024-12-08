fb556e1c551daadabadf9e95e05ca826448df05e
alrkunggul <62210942+alrkunggul@users.noreply.github.com>
2024-12-08 22:25:22 +0700

    Update neuron_hidden.v

--- Level1/neuron_hidden.v
+++ Level1/neuron_hidden.v
@@ -2,7 +2,7 @@ module neuron_hidden(
     input wire [19:0] mean,    // Mean pertama
     input wire [19:0] dev,
 
-    output wire [19:0] a2,    // Latent variable pertama
+    output wire [19:0] a2  // Latent variable pertama
 );
     // Reparameterization trick: a2 = mean + (softplus * epsilon)
     assign a2 = mean + dev;
