0f0d3d6e82a5a898347e9871b8a03e6453f18755
abe-pratomo <90553934+abe-pratomo@users.noreply.github.com>
2024-12-08 21:01:10 +0700

    Updated mean.v

--- Level1/mean.v
+++ Level1/mean.v
@@ -5,3 +5,15 @@ module mean(
 
     output wire signed [19:0] C_out // Output from mean module
 );
+
+    assign C_out = ((X_0) ? Wm_0 : 0) + // Simple conditional check whether each pixel is 1 or 0
+                   ((X_1) ? Wm_1 : 0) +
+                   ((X_2) ? Wm_2 : 0) +
+                   ((X_3) ? Wm_3 : 0) +
+                   ((X_4) ? Wm_4 : 0) +
+                   ((X_5) ? Wm_5 : 0) +
+                   ((X_6) ? Wm_6 : 0) +
+                   ((X_7) ? Wm_7 : 0) +
+                   ((X_8) ? Wm_8 : 0) + B_mean;
+
+endmodule
